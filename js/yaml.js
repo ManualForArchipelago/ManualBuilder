788a81754512c99e01c8355d28234be18aa417e2
Fuzzy <slightlyfuzzybeard@gmail.com>
2023-08-03 23:37:17 -0500

    replace non-alphanum in yaml too

--- js/yaml.js
+++ js/yaml.js
@@ -11,15 +11,18 @@ export class Yaml {
     toYaml() {
         if (!this.vue || !this.vue.game || !this.vue.creator) return;
 
+        let game = this.vue.game.replace(/[^A-Za-z0-9]/g, '');
+        let creator = this.vue.creator.replace(/[^A-Za-z0-9]/g, '');
+
         const yamlContents = 
-            `Manual_${this.vue.game}_${this.vue.creator}:\n` +
+            `Manual_${game}_${creator}:\n` +
             '    progression_balancing: 50\n' +
             '    accessibility: items\n\n' +
-            `game: Manual_${this.vue.game}_${this.vue.creator}\n` +
-            `name: ${this.vue.creator}`;
+            `game: Manual_${game}_${creator}\n` +
+            `name: ${creator}`;
 
         const file = new Blob([yamlContents], { type: 'text/yaml' });
 
-        saveAs(file, `Manual_${this.vue.game}_${this.vue.creator}.yaml`);
+        saveAs(file, `Manual_${game}_${creator}.yaml`);
     }
 }
