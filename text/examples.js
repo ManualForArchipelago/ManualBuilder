b27a6eba30d2cf85d6778dd54305e892910f7ec7
Fuzzy <slightlyfuzzybeard@gmail.com>
2024-09-29 11:53:22 -0500

    move tooltip and example text into centralized files for non-technical management/updating

---
+++ text/examples.js
@@ -0,0 +1,18 @@
+export const Examples = {
+    SIDEBAR_GAME: "",
+    SIDEBAR_CREATOR: "your name, probably",
+    SIDEBAR_FILLER_NAME: "",
+
+    ITEMS_NAME: "",
+    ITEMS_CATEGORIES: "Category 1, Category 2, etc.",
+    ITEMS_COUNT: "",
+
+    LOCATIONS_NAME: "",
+    LOCATIONS_CATEGORIES: "Category 1, Category 2, etc.",
+    LOCATIONS_REQUIREMENTS: "e.g., |Master Sword| AND (|Lamp| OR |Fire Rod|) AND (|Crystal:7|)",
+    LOCATIONS_PLACE_ITEM: "Match 1, Match 2, etc.",
+
+    REGIONS_NAME: "",
+    REGIONS_CONNECTS_TO: "Region 1, Region 2, etc.",
+    REGIONS_REQUIREMENTS: "e.g., |Master Sword| AND (|Lamp| OR |Fire Rod|) AND (|Crystal:7|)"
+};
